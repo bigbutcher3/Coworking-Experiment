ee8e2922972ac300bb5c82dd703e2543e74208b9
Puschelus <puschelus@outlook.de>
2024-06-20 13:27:41 +0200

    Update Program.cs
    A

--- Coworking-Experiment/Program.cs
+++ Coworking-Experiment/Program.cs
@@ -5,9 +5,11 @@ namespace CoworkingExperiment
     class Program
     {
         static void Main(string[] args)
+        public static double Subtraktion(double a, double b)
         {
             Console.WriteLine("Hello World!");
             Console.ReadKey();
+            return a - b;
         }
     }
 }
