720f0c91f22192177602b4f2c41591e12c55fbad
Mischka1977 <Zunke.thomas@web4train.de>
2024-06-20 14:26:26 +0200

    DivisionVersion2

--- Coworking-Experiment/Division.cs
+++ Coworking-Experiment/Division.cs
@@ -4,17 +4,17 @@ namespace CoworkingExperiment
 {
     class Division
     {
-         public static double Div(double dividend, double divisor)
+         public static double Div(double a, double b)
         {
 
     
             
-                if (divisor == 0)
+                if (a == 0)
                 {
                     throw new ArgumentException("Die Division darf nicht durch null sein.");
                 }
 
-                return dividend / divisor;
+                return a / b;
         }
     }
 
